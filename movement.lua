c588266b6ab7907d060703e4dfc5cfaf1bc82f67
marius schlueter <themrslue@googlemail.com>
2025-01-08 03:09:23 +0100

    fix

--- movement.lua
+++ movement.lua
@@ -164,6 +164,7 @@ function gps_to_local(x,y,z)
     loc_dx,loc_dz = dx,dz
     loc_dz = -loc_dz
     print("dx,dz "..dx.." "..dz)
+    rot_cnt = rot_cnt + 2
     for i=1,rot_cnt do
         loc_dx,loc_dz = rotate_vec_left(loc_dx,loc_dz)
         print("rotated dx,dz ",loc_dx,loc_dz)
