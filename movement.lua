2c78a719793daaf766548c151e2edecbb5e61239
marius schlueter <themrslue@googlemail.com>
2025-01-08 02:31:18 +0100

    fix

--- movement.lua
+++ movement.lua
@@ -168,7 +168,7 @@ function gps_to_local(x,y,z)
         print("rotated dx,dz "..loc_dx.." "..loc_dz)
     end
 
-    return loc_dx,loc_dz,(dy + current_y)
+    return loc_dx,loc_dz,(dy + current_z)
 end
 
 function move_to_gps(x,y,z)
