d1e67b30eebccb32401c63fefc5083352e116077
marius schlueter <themrslue@googlemail.com>
2025-01-10 02:52:13 +0100

    fix

--- download.lua
+++ download.lua
@@ -11,8 +11,41 @@ files = {
     "remote_control",
     "logistic"
 }
+
+local function get(sFile)
+
+    local cacheBuster = ("%x"):format(math.random(0, 2 ^ 30))
+    local response, err = http.get(
+        "https://raw.githubusercontent.com/kmodexc/cc_scripts/refs/heads/main/" .. (sFile) .. "?cb=" .. cacheBuster
+    )
+
+    if not response then
+        io.stderr:write("Failed.\n")
+        print(err)
+        return
+    end
+
+    print("Success.")
+
+    local res = response.readAll()
+    response.close()
+
+    local sPath = shell.resolve(sFile)
+
+    if res then
+        local file = fs.open(sPath, "w")
+        file.write(res)
+        file.close()
+
+        print("Downloaded as " .. sFile)
+    end
+end
+
+
+
 for k,v in pairs(files) do
     shell.run("rm "..v..".lua")
     cacheBuster = ("%x"):format(math.random(0, 2 ^ 30))
-    shell.run("wget https://raw.githubusercontent.com/kmodexc/cc_scripts/refs/heads/main/"..v..".lua?cb="..cacheBuster)
+    --shell.run("wget "..v..".lua?cb="..cacheBuster)
+    get(v..".lua")
 end
