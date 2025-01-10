74f462172701942690f4caf5af53cd330c129bfa
marius schlueter <themrslue@googlemail.com>
2025-01-10 02:42:32 +0100

    fix

--- logistic.lua
+++ logistic.lua
@@ -75,7 +75,7 @@ function controller()
     free_chests = {}
     for i=1,num_chests do
         free_chests[i] = {}
-        free_chests[i]["pos"] = storage_pos.add(vector.new(i-1,0,0))
+        free_chests[i]["pos"] = storage_pos:add(vector.new(i-1,0,0))
         free_chests[i]["ori"] = storage_ori
         free_chests[i]["items"] = 0
     end
