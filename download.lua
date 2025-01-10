00a8c1d5653d539c9df8b35f42a401cbc3f304d4
marius schlueter <themrslue@googlemail.com>
2025-01-10 01:31:47 +0100

    fix

--- download.lua
+++ download.lua
@@ -1,4 +1,16 @@
-files = {"baum","composter","farm","line","makeroom","movement","mytunnel","puttop","savetunnel"}
+files = {
+    "baum",
+    "composter",
+    "farm",
+    "line",
+    "makeroom",
+    "movement",
+    "mytunnel",
+    "puttop",
+    "savetunnel",
+    "remote_control",
+    "logistic"
+}
 for k,v in pairs(files) do
     shell.run("rm "..v..".lua")
     shell.run("wget https://raw.githubusercontent.com/kmodexc/cc_scripts/refs/heads/main/"..v..".lua")
